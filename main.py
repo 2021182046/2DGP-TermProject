b02364c1f03ce84b762c479fa8d70c8b6c7bd9a0
2021182046 <lope32@naver.com>
2023-10-25 21:57:10 +0900

    Update move_control
    플레이어 상태에 따른 함수활용 컨트롤 추가

--- main.py
+++ main.py
@@ -16,13 +16,77 @@ ui_lap_0 = load_image('UI_Lap0.png')
 ui_lap_1 = load_image('UI_Lap1.png')
 ui_lap_2 = load_image('UI_Lap2.png')
 
+idle, move = True, False
+dir, dir_y = 0,0
+player_x, player_y = WIDTH//2, HEIGHT//2
+
+
+def move_event():
+    global idle, move, dir, dir_y
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            move = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_LEFT:
+                idle = False
+                move = True
+                dir -= 1
+            elif event.key == SDLK_RIGHT:
+                idle = False
+                move = True
+                dir += 1
+            elif event.key == SDLK_UP:
+                idle = False
+                move = True
+                dir_y += 1
+            elif event.key == SDLK_DOWN:
+                idle = False
+                move = True
+                dir_y -= 1
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_LEFT:
+                idle = True
+                move = False
+                dir += 1
+            elif event.key == SDLK_RIGHT:
+                idle = True
+                move = False
+                dir -= 1
+            elif event.key == SDLK_UP:
+                idle = True
+                move = False
+                dir_y -= 1
+            elif event.key == SDLK_DOWN:
+                idle = True
+                move = False
+                dir_y += 1
+
+
 while(True):
-    clear_canvas()
-    map.draw_now(WIDTH//2, HEIGHT//2)
-    ui_main.draw_now(WIDTH//2, HEIGHT//2)
-    ui_1st.draw_now(80, HEIGHT-50)
-    ui_lap.draw_now(250, HEIGHT-50)
-    ui_lap_0.draw_now(360, HEIGHT-50)
-    player.draw_now(WIDTH//2, HEIGHT//2, 80, 50) # 원본 크기 변경하여 적용
-    update_canvas()
-    delay(0.1)
+    while idle:
+        clear_canvas()
+        map.draw_now(WIDTH//2, HEIGHT//2)
+        ui_main.draw_now(WIDTH//2, HEIGHT//2)
+        ui_1st.draw_now(80, HEIGHT-50)
+        ui_lap.draw_now(250, HEIGHT-50)
+        ui_lap_0.draw_now(360, HEIGHT-50)
+        player.draw_now(player_x, player_y, 80, 50) # 원본 크기 변경하여 적용
+        update_canvas()
+        move_event()
+        delay(0.1)
+
+    while move:
+        clear_canvas()
+        map.draw_now(WIDTH//2, HEIGHT//2)
+        ui_main.draw_now(WIDTH // 2, HEIGHT // 2)
+        ui_1st.draw_now(80, HEIGHT - 50)
+        ui_lap.draw_now(250, HEIGHT - 50)
+        ui_lap_0.draw_now(360, HEIGHT - 50)
+        player.draw_now(player_x, player_y, 80, 50)  # 원본 크기 변경하여 적용
+        update_canvas()
+        move_event()
+        player_x += dir*5
+        player_y += dir_y*5
+        delay(0.1)
