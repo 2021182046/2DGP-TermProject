0f4c5f0bae3d664493624991c5e7efb1e0093f88
2021182046 <lope32@naver.com>
2023-10-25 21:22:01 +0900

    Update UI
    화면 해상도 변경에 따른 UI 수정

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from pico2d import *
 
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 1280, 720
 open_canvas(WIDTH, HEIGHT)
 
 map = load_image('map0.png')
 player = load_image('player_car.png')
 
-ui_main = load_image('UI_black(800x600).png')
+ui_main = load_image('UI_black(1280x720).png')
 ui_1st = load_image('UI_1st.png')
 ui_2nd = load_image('UI_2nd.png')
 ui_3rd = load_image('UI_3rd.png')
@@ -20,9 +20,9 @@ while(True):
     clear_canvas()
     map.draw_now(WIDTH//2, HEIGHT//2)
     ui_main.draw_now(WIDTH//2, HEIGHT//2)
-    ui_1st.draw_now(60, HEIGHT-40)
-    ui_lap.draw_now(250, HEIGHT-40)
-    ui_lap_0.draw_now(360, HEIGHT-40)
+    ui_1st.draw_now(80, HEIGHT-50)
+    ui_lap.draw_now(250, HEIGHT-50)
+    ui_lap_0.draw_now(360, HEIGHT-50)
     player.draw_now(WIDTH//2, HEIGHT//2, 80, 50) # 원본 크기 변경하여 적용
     update_canvas()
     delay(0.1)
