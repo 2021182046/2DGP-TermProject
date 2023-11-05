f4beaae360191fec67a40ca8a30fe7b3922da411
2021182046 <lope32@naver.com>
2023-11-06 03:31:39 +0900

    Update move_rotate
    기존 코드 삭제 후 클래스 활용 재배치, 좌우 각도조절 추가

--- main.py
+++ main.py
@@ -17,30 +17,34 @@ ui_lap_0 = load_image('UI_Lap0.png')
 ui_lap_1 = load_image('UI_Lap1.png')
 ui_lap_2 = load_image('UI_Lap2.png')
 
-idle, move, on_break = True, False, False
+idle, move = True, False
+left, right = False, False
 dir, dir_y = 0,0
 player_x, player_y = WIDTH//2, HEIGHT//2
-speed, speed_limit = 0, 10
-accel, accel_break, break_abs = 0.5, 0.5, 0
 
+class Car:
+    Image_player = player
 
-def speed_down():
-    global speed, accel_break
-    if speed > 0:
-        speed -= accel_break
-        delay(0.01)
-        if speed < 0:
-            speed = 0
-            move = False
+    def __init__(self, speed_limit, rotation): #속도, 각도
+        self.image = self.Image_player
+        self.speed = 0
+        self.speed_limit = speed_limit
+        self.rotation_angle = 0
+        self.rotation = rotation
 
-def speed_up():
-    global speed, accel
-    if speed < speed_limit:
-        speed += accel
+    def rotate(self, left, right):
+        if left:
+            self.rotation_angle += self.rotation
+        elif right:
+            self.rotation_angle -= self.rotation
+
+    def draw(self):
+        self.image.rotate_draw(self.rotation_angle, player_x, player_y, 80, 50)
+
+PLAYER_CAR = Car(50, 1)
 
 def move_event():
-    global idle, move, dir, dir_y
-    global speed, accel
+    global idle, move, dir, dir_y, PLAYER_CAR
 
     events = get_events()
     for event in events:
@@ -50,76 +54,52 @@ def move_event():
             if event.key == SDLK_a:
                 idle = False
                 move = True
+                PLAYER_CAR.rotate(True, False)
                 dir -= 1
             elif event.key == SDLK_d:
                 idle = False
                 move = True
+                PLAYER_CAR.rotate(False, True)
                 dir += 1
             elif event.key == SDLK_w:
                 idle = False
                 move = True
                 dir_y += 1
-                speed_up()
             elif event.key == SDLK_s:
                 idle = False
                 move = True
                 dir_y -= 1
-                speed_down()
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_a:
                 idle = True
                 move = False
+                PLAYER_CAR.rotate(False, False)
                 dir += 1
             elif event.key == SDLK_d:
                 idle = True
                 move = False
+                PLAYER_CAR.rotate(False, False)
                 dir -= 1
             elif event.key == SDLK_w:
                 idle = True
                 move = False
-                on_break = True
                 dir_y -= 1
             elif event.key == SDLK_s:
                 idle = True
                 move = False
                 dir_y += 1
-                speed_down()
 
 
 while(True):
-    while idle:
-        clear_canvas()
-        map.draw_now(WIDTH//2, HEIGHT//2)
-        ui_main.draw_now(WIDTH//2, HEIGHT//2)
-        ui_1st.draw_now(80, HEIGHT-50)
-        ui_lap.draw_now(250, HEIGHT-50)
-        ui_lap_0.draw_now(360, HEIGHT-50)
-        player.draw_now(player_x, player_y, 80, 50) # 원본 크기 변경하여 적용
-        player_x += dir*3
-        player_y += dir_y*3 + speed
-        update_canvas()
-        move_event()
-        if on_break:
-            break_abs += 1
-            if break_abs > 10:
-                on_break = False
-                break_abs = 0
-            else:
-                while 10:
-                    speed_down()
-                    delay(0.01)
-        delay(0.05)
-
-    while move:
-        clear_canvas()
-        map.draw_now(WIDTH//2, HEIGHT//2)
-        ui_main.draw_now(WIDTH // 2, HEIGHT // 2)
-        ui_1st.draw_now(80, HEIGHT - 50)
-        ui_lap.draw_now(250, HEIGHT - 50)
-        ui_lap_0.draw_now(360, HEIGHT - 50)
-        player.draw_now(player_x, player_y, 80, 50)  # 원본 크기 변경하여 적용
-        player_x += dir * 3
-        player_y += dir_y * 3 + speed
-        update_canvas()
-        move_event()
-        delay(0.05)
+    clear_canvas()
+    map.draw_now(WIDTH//2, HEIGHT//2)
+    ui_main.draw_now(WIDTH // 2, HEIGHT // 2)
+    ui_1st.draw_now(80, HEIGHT - 50)
+    ui_lap.draw_now(250, HEIGHT - 50)
+    ui_lap_0.draw_now(360, HEIGHT - 50)
+    #player.draw_now(player_x, player_y, 80, 50)  # 원본 크기 변경하여 적용
+    PLAYER_CAR.draw()
+    player_x += dir * 3
+    player_y += dir_y * 3
+    update_canvas()
+    move_event()
